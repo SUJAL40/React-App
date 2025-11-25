05fd2b71062dd0199f83de6eae010c619f79f919
SUJAL40 <sujalkhot2479@gmail.com>
2025-11-25 18:18:22 +0530

    React Component
    Here learn what is component in React. How to Make function in React etc

---
+++ React Basic Code/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Title from './Title.jsx';
+import ProductTab from './ProductTab.jsx'
+
+function Description(){
+  return <p>This is a description paragraph.</p>;
+}
+
+function App() {
+    return (
+      <ProductTab />
+    );    
+}
+
+export default App;
