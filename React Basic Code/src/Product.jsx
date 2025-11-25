fd8c10be2e11f142f1c137ccf10075ca98dd12b7
SUJAL40 <sujalkhot2479@gmail.com>
2025-11-25 19:56:01 +0530

    Props,Passing Array to Props, Rrendering Arrays
    📌 Props
    
    Props are read-only inputs passed from parent to child components to make components reusable and dynamic.
    
    📌 Passing Array to Props
    
    Arrays can be passed as props just like any other value, allowing components to render lists or structured data.
    
    📌 Rendering Arrays
    
    To render an array in React, map over it and return JSX for each item, ensuring each element has a unique key prop.

--- React Basic Code/src/Product.jsx
+++ React Basic Code/src/Product.jsx
@@ -1,10 +1,14 @@
 import './Product.css';
 
-function Product(){
+// Ensure the props are destructured correctly
+function Product({title , price, feature, features}){
+    const list = feature.map((feature)=> <li>{feature}</li>)
     return (
     <div className="Product">
-        <h3>Product Title</h3>
-        <h5>Product Description</h5>
+        <h3>{title}</h3>
+        <h5>Product Price : {price}</h5>
+        <h5>Feature(Array): {list}</h5>
+        <h5>Feature(Object): {features.a}</h5>
     </div>
     );
 }
--- React Basic Code/src/ProductTab.jsx
+++ React Basic Code/src/ProductTab.jsx
@@ -1,10 +1,14 @@
 import Product from "./Product.jsx";
 
 function ProductTab(){
+  let option = ["hi-tech ", "durable ", "fast"];
+  let opt2 = {a: "hi-tech ", b: " durable"};
+
     return (
       <div>
-        <Product />
-        <Product />
+        <Product title="Phone" price={30000} feature={option} features={opt2}/>
+        <Product title="Laptop" price={60000} feature={option} features={opt2}/>
+        <Product title="Tab" price={50000} feature={option} features={opt2}/>
       </div>
     );  
 }
