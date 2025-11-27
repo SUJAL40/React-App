b37595fe7f95324d890c53d787392cc14ec5c1a3
SUJAL40 <sujalkhot2479@gmail.com>
2025-11-27 23:18:51 +0530

    Radha Naam Jap Counter
    useState() is a React Hook that allows functional components to create and manage state. It returns a state value and a function to update that value, and whenever the state changes, the component automatically re-renders.

---
+++ React-State-Class/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Counter from './Counter';
+
+function App() {
+  return (
+  <>
+  <h1>राधा नाम जप</h1>
+  <h3>(States in React)</h3>
+  <Counter />
+  </>
+);
+}
+
+export default App
