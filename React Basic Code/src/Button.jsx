0c66773adac5dd116f58680538e9d3e9c225a46b
SUJAL40 <sujalkhot2479@gmail.com>
2025-11-27 20:55:06 +0530

    Handling Non-Click Events
    Handling Non-Click Events means responding to user actions other than clicking, such as typing, hovering, scrolling, loading, or pressing keys. These events help improve user interaction by detecting changes in input, movement, or page behavior.

--- React Basic Code/src/Button.jsx
+++ React Basic Code/src/Button.jsx
@@ -1,11 +1,25 @@
-function sayRadha(){
+function handleClick(){
     console.log("Radha Krishna");
+}
+
+function handleHover(){
+    console.log("You hovered over the Paragraph");
+}
+
+function DoubleClick(){
+    console.log("You double clicked the button");
 }
 
 export default function Button(){
     return (
         <div>
-            <button onClick={sayRadha}>Click Me!</button>
+            <button onClick={handleClick}>Click Me!</button>
+            <p onMouseOver={handleHover} >This is random paragraph
+                It changes on hover.
+                It checks the hover event.
+                if you hover over this text, you will see a message in console.
+            </p>
+            <button onDoubleClick={DoubleClick}>Double Click me!</button>
         </div>
         
     );
