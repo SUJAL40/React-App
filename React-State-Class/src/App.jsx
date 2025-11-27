707e2ed5aa05acd31a962b821b3c324e9fce8096
SUJAL40 <sujalkhot2479@gmail.com>
2025-11-27 23:52:58 +0530

    Like Button With Jup Counter
    Introduced the LikeButton component, which handles the toggle state and display logic for liking/unliking content.

--- React-State-Class/src/App.jsx
+++ React-State-Class/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Counter from './Counter';
+import Like from './LIkeButton';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
   <h1>राधा नाम जप</h1>
   <h3>(States in React)</h3>
   <Counter />
+  <Like />
   </>
 );
 }
