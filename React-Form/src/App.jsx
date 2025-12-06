b0b2108d4f2901bece003a0741e04e1c51888df4
SUJAL40 <sujalkhot2479@gmail.com>
2025-12-06 11:58:11 +0530

    Implement Counter component with basic useEffect
    Created the Counter component using useState to track the count. Implemented a basic useEffect hook (without a dependency array) to demonstrate side effects running after every render.

--- React-Form/src/App.jsx
+++ React-Form/src/App.jsx
@@ -1,12 +1,13 @@
 import Form from './Form'
 import './App.css'
 import CommentsForm from './CommentsForm'
+import Counter from './Counter'
 
 function App() {
 
   return (
     <>
-      <CommentsForm />
+      <Counter />
     </>
   )
 }
