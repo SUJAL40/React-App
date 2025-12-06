269ce4298d356999fc90dd5a9e9615f605e905ca
SUJAL40 <sujalkhot2479@gmail.com>
2025-12-06 12:06:13 +0530

    Add dependencies to useEffect for controlled execution
    Updated the useEffect hook to include a dependency array, ensuring the side effect only runs when the values in the array (state or props) change, optimizing performance and preventing unnecessary re-runs.

--- React-Form/src/Counter.jsx
+++ React-Form/src/Counter.jsx
@@ -1,21 +1,29 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 
-export default function Counter(){
+export default function Counter() {
 
-    let [count,setCount]= useState(0);
-    
-    let incCount = () =>{
-        setCount((currCount)=>currCount+1);
+    let [countx, setCountx] = useState(0);
+    let [county, setCounty] = useState(0);
+
+    let incCountx = () => {
+        setCountx((currCount) => currCount + 1);
     };
 
-    useEffect(function printSomething(){
-        console.log("this is a side effect");
-    })
+    let incCounty = () => {
+        setCounty((currCount) => currCount + 1);
+    };
 
-    return(
+    useEffect(function printSomething() {
+        console.log("this is a side effect");
+    },
+    [countx])
+
+    return (
         <div>
-            <h3>Count = {count}</h3>
-            <button onClick={incCount}>+1</button>
+            <h3>Countx = {countx}</h3>
+            <button onClick={incCountx}>+1</button>
+             <h3>County = {county}</h3>
+            <button onClick={incCounty}>+1</button>
         </div>
     );
 }
